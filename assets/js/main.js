d1860baaa28fd83154d049305ab6f022c5aff326
patelAnkita215 <>
2025-05-20 16:28:12 +0530

    add new section

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,48 @@ btn.onclick = function () {
     btnst = true;
   }
 };
+
+// slider
+$(document).ready(function(){
+  $('#logo-marquee').slick({
+     dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 8000,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1320,
+          settings: {
+            slidesToShow: 4,
+            speed: 12000,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 1080,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 680,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+      }]
+  });
+});
